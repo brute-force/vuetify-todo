df6cb395f84be702b472a2dd296745109c3b1d0c
Reuel Alvarez <reuel.alvarez@gmail.com>
2019-10-15 10:31:29 -0400

    move nav bar to every view except login

---
+++ src/store.js
@@ -0,0 +1,225 @@
+
+import Vue from 'vue';
+import Vuex from 'vuex';
+import Cookies from 'js-cookie';
+import moment from 'moment';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    token: Cookies.get('token') || '',
+    user: JSON.parse(Cookies.get('user') || '{}'),
+    tasks: [],
+    myTasks: [],
+    snackbar: {
+      title: '',
+      doShow: false
+    }
+  },
+  mutations: {
+    login (state, { token, user }) {
+      state.token = token;
+      state.user = user;
+    },
+    logout (state) {
+      state.token = '';
+      state.user = {};
+    },
+    setTasks (state, tasks) {
+      state.tasks = tasks;
+    },
+    setMyTasks (state, tasks) {
+      state.myTasks = tasks;
+    },
+    createTask (state, task) {
+      state.myTasks.push(task);
+    },
+    deleteTask (state, id) {
+      const index = state.myTasks.findIndex((task) => task._id === id);
+
+      if (index !== -1) {
+        state.myTasks.splice(index, 1);
+      }
+    },
+    updateTask (state, task) {
+      const index = state.myTasks.findIndex((_task) => _task._id === task._id);
+
+      if (index !== -1) {
+        state.myTasks.splice(index, 1, task);
+      }
+    },
+    updateSnackbar (state, snackbar) {
+      state.snackbar = snackbar;
+    }
+  },
+  actions: {
+    register ({ commit }, user) {
+      return new Promise((resolve, reject) => {
+        this.$http.post('/users', { name: user.name, email: user.email, password: user.password })
+          .then((res) => {
+            const { token, user: { name, _id } } = res.data;
+            user.name = name;
+            user.id = _id;
+            delete user.password;
+
+            // set secure to true when deploying to an https environment
+            // const opts = { expires: 1, samesite: 'Strict', secure: true };
+            const opts = { expires: 1, samesite: 'Strict' };
+
+            Cookies.set('token', token, opts);
+            Cookies.set('user', JSON.stringify(user), opts);
+
+            this.$http.defaults.headers.common.Authorization = token;
+
+            commit('login', { token, user });
+
+            resolve(res);
+          })
+          .catch((err) => {
+            reject(err);
+          });
+      });
+    },
+    login ({ commit }, user) {
+      return new Promise((resolve, reject) => {
+        this.$http.post('/users/login', { email: user.email, password: user.password })
+          .then((res) => {
+            const { token, user: { name, _id } } = res.data;
+            user.name = name;
+            user.id = _id;
+            delete user.password;
+
+            // set secure to true when deploying to an https environment
+            // const opts = { expires: 1, samesite: 'Strict', secure: true };
+            const opts = { expires: 1, samesite: 'Strict' };
+
+            Cookies.set('token', token, opts);
+            Cookies.set('user', JSON.stringify(user), opts);
+
+            this.$http.defaults.headers.common.Authorization = token;
+
+            commit('login', { token, user });
+
+            resolve(res);
+          })
+          .catch((err) => {
+            reject(err);
+          });
+      });
+    },
+    logout ({ commit }) {
+      return new Promise((resolve, reject) => {
+        this.$http.get('/users/logout')
+          .then((res) => {
+            Cookies.remove('token');
+            Cookies.remove('user');
+
+            delete this.$http.defaults.headers.common.Authorization;
+
+            commit('logout');
+
+            resolve('logged out');
+          })
+          .catch((err) => {
+            reject(err);
+          });
+      });
+    },
+    getAllTasks ({ commit }) {
+      return new Promise((resolve, reject) => {
+        this.$http.get('/tasks/all')
+          .then((res) => {
+            commit('setTasks', res.data);
+
+            resolve(res.data);
+          })
+          .catch((err) => {
+            reject(err);
+          });
+      });
+    },
+    getMyTasks ({ commit }) {
+      return new Promise((resolve, reject) => {
+        this.$http.get('/tasks?sortBy=dateDue:desc')
+          .then((res) => {
+            commit('setMyTasks', res.data);
+
+            resolve(res.data);
+          })
+          .catch((err) => {
+            reject(err);
+          });
+      });
+    },
+    createTask ({ commit }, task) {
+      return new Promise((resolve, reject) => {
+        this.$http.post('/tasks', task)
+          .then((res) => {
+            commit('createTask', res.data);
+
+            resolve(res.data);
+          })
+          .catch((err) => {
+            reject(err);
+          });
+      });
+    },
+    deleteTask ({ commit }, id) {
+      return new Promise((resolve, reject) => {
+        this.$http.delete(`/tasks/${id}`)
+          .then((res) => {
+            commit('deleteTask', id);
+
+            resolve(res.data);
+          })
+          .catch((err) => {
+            reject(err);
+          });
+      });
+    },
+    updateTask ({ commit }, { _id, ...task }) {
+      return new Promise((resolve, reject) => {
+        this.$http.patch(`/tasks/${_id}`, task)
+          .then((res) => {
+            task._id = _id;
+            commit('updateTask', task);
+
+            resolve(res.data);
+          })
+          .catch((err) => {
+            reject(err);
+          });
+      });
+    },
+    updateSnackbar ({ commit }, snackbar) {
+      commit('updateSnackbar', snackbar);
+    }
+  },
+  getters: {
+    token: state => state.token,
+    user: state => state.user,
+    allTasks: state => state.tasks,
+    myTasks: state => state.myTasks.sort((a, b) => {
+      if (a.isCompleted < b.isCompleted) {
+        return -1;
+      } else if (a.isCompleted > b.isCompleted) {
+        return 1;
+      } else {
+        const momentA = moment(a.dateDue, 'YYYY-MM-DDTHH:mm:ss.SSSZ');
+        const momentB = moment(b.dateDue, 'YYYY-MM-DDTHH:mm:ss.SSSZ');
+
+        if (momentA.isBefore(momentB)) {
+          return -1;
+        } else if (momentA.isAfter(momentB)) {
+          return 1;
+        } else {
+          return 0;
+        }
+      }
+    }),
+    snackbar: state => state.snackbar
+    // tasksByUserId: state => id => state.tasks.filter((task) => task.owner._id === id),
+    // myTasks: state => state.tasks.filter((task) => task.owner._id === state.user.id)
+  }
+});
