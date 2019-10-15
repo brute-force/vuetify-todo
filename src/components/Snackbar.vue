67b92ff18793f33e28873a03cff5ad1ea935f4af
Reuel Alvarez <reuel.alvarez@gmail.com>
2019-10-15 10:35:03 -0400

    snackbar component for alert display after create/update/delete tasks

---
+++ src/components/Snackbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-snackbar
+    v-model="snackbar.doShow"
+    :timeout="3000"
+    top
+    color="success"
+  >
+    <span>{{ snackbar.title }}</span>
+    <v-btn
+      text
+      color="white"
+      @click="updateSnackbar({ title: '', doShow: false })"
+    >
+      close
+    </v-btn>
+  </v-snackbar>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex';
+
+export default {
+  computed: {
+    ...mapGetters(['snackbar'])
+  },
+  methods: {
+    ...mapActions(['updateSnackbar'])
+  }
+};
+</script>
