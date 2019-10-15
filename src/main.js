07d67e27d9d46e7fc7f5b74dbb8e8e517f92e01c
Reuel Alvarez <reuel.alvarez@gmail.com>
2019-10-15 10:42:38 -0400

    add store, router, and handle cookies and tokens if logged in

--- src/main.js
+++ src/main.js
@@ -1,10 +1,46 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import vuetify from './plugins/vuetify';
+import Vuex from 'vuex';
+import axios from 'axios';
+import Cookies from 'js-cookie';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+const api = axios.create({
+  baseURL: 'http://192.168.1.62:3000'
+});
+
+api.interceptors.response.use((response) => {
+  return response;
+}, (err) => {
+  if (err.response.status === 401) {
+    // delete cookies and axios authorization header if unauthorized
+    Cookies.remove('token');
+    Cookies.remove('user');
+
+    // console.log(!!api.defaults.headers.common.Authorization);
+
+    delete api.defaults.headers.common.Authorization;
+  }
+
+  return Promise.reject(err.response);
+});
+
+const token = Cookies.get('token');
+
+if (token) {
+  api.defaults.headers.common.Authorization = token;
+}
+
+Vue.prototype.$http = api;
+Vuex.Store.prototype.$http = api;
 
 new Vue({
   router,
+  store,
+  vuetify,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
